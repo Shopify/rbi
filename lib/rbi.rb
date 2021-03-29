78a0aa1da329d76f1d7a2dd828e5c5d2ab9f392f
Kaan Ozkan <kaan.ozkan@shopify.com>
2021-03-29 16:10:29 -0400

    bundle gem rbi

---
+++ lib/rbi.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rbi/version"
+
+module Rbi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rbi/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rbi
+  VERSION = "0.1.0"
+end
