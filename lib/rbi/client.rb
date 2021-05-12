f18f98bbfd51ee8c0c415af68d78027812386dda
Kaan Ozkan <kaan.ozkan@shopify.com>
2021-05-12 16:23:37 -0400

    iterate over direct dependencies instead of lockfile

--- lib/rbi/client.rb
+++ lib/rbi/client.rb
@@ -7,6 +7,7 @@ module RBI
 
     CENTRAL_REPO_SLUG = "shopify/rbi"
     GEM_RBI_DIRECTORY = "sorbet/rbi/gems"
+    IGNORED_GEMS = T.let(%w{bundler sorbet sorbet-static sorbet-runtime tapioca}.freeze, T::Array[String])
 
     sig { params(logger: Logger, github_client: T.nilable(GithubClient), project_path: String).void }
     def initialize(logger, github_client: nil, project_path: ".")
@@ -63,22 +64,31 @@ module RBI
 
     sig { params(gems: T::Array[String]).void }
     def generate(gems)
-      # TODO: Skip if RBI already exists locally (Thus no need to "merge" inside generate command)
+      # TODO: Skip if RBI already exists locally (Thus don't "merge" inside generate command)
       if gems.empty?
-        file = Bundler.read_file("#{@project_path}/Gemfile.lock")
-        parser = Bundler::LockfileParser.new(file)
-        # TODO: Don't iterate over lockfile, iterate over direct dependencies only
-        parser.specs.each do |spec|
+        deps = Bundler.locked_gems.dependencies # All direct dependencies of the application
+        deps.values.each do |dep|
+          next if IGNORED_GEMS.include?(dep.name)
+          next if dep.name == "rbi" # TODO: Remove, can't install internal gems currently
+          next if dep.name == "test" # TODO: Remove, direct dependencies returns the application itself, is there an alternative?
+
+          if dep.to_specs.size > 1
+            @logger.error("Unexpected")
+            exit
+          end
+          spec = dep.to_specs.first # Convert dependency to a spec to access name and version
           name = spec.name
           version = spec.version.to_s
           path = @repo.rbi_path(name, version)
           if path
-            @logger.error("The RBI for `#{name}@#{version}` gem already exists in the central repository.")
+            @logger.error("The RBI for `#{name}@#{version}` gem already exists in the central repository and won't be generated locally.")
             @logger.hint("Run `rbi update` to get it.")
             next
+          elsif has_local_rbi_for_gem_version?(name, version)
+            next # TODO display warning and inform of a `--force` option to regenerate even if it exists
           end
 
-          tapioca_generate(name, version)
+          tapioca_generate(name, version, spec.dependencies)
         end
       else
         gems.each do |gem|
@@ -87,11 +97,18 @@ module RBI
             @logger.error("Argument to `rbi generate` is in the wrong format. Please pass in `gem_name@gem_version`.")
             next
           end
+          if IGNORED_GEMS.include?(name)
+            @logger.info("RBI generation for #{name}@#{version} is skipped as it's not necessary")
+            next
+          end
+
           path = @repo.rbi_path(T.must(name), T.must(version))
           if path
             @logger.error("The RBI for `#{name}@#{version}` gem already exists in the central repository.")
             @logger.hint("Run `rbi update` to get it.")
             next
+          elsif has_local_rbi_for_gem_version(name, version)
+            next # TODO display warning and inform of a `--force` option to regenerate even if it exists
           end
 
           tapioca_generate(name, version)
@@ -134,7 +151,7 @@ module RBI
       end
     end
 
-    def tapioca_generate(name, version)
+    def tapioca_generate(name, version, dependencies = nil)
       # Create directory (tmp)
       ctx = Context.new("/tmp/rbi/gems/#{name}/#{version}")
       ctx.sorbet_config(".")
@@ -146,27 +163,44 @@ module RBI
         gem("tapioca")
       GEMFILE
 
-      # TODO: Error handling
       Bundler.with_unbundled_env do
-        out, err, status = ctx.run("bundle config set --local path 'vendor/bundle'")
-        out, err, status = ctx.run("bundle", "install")
-        out, err, status = ctx.run("bundle", "exec", "tapioca", "generate")
+        ctx.run("bundle config set --local path 'vendor/bundle'")
+        _, err, status = ctx.run("bundle", "install")
+        unless status
+          @logger.error("Unable to generate RBI: #{err}")
+          ctx.destroy
+          exit
+        end
+        _, err, status = ctx.run("bundle", "exec", "tapioca", "generate")
+        unless status
+          @logger.error("Unable to generate RBI: #{err}")
+          ctx.destroy
+          exit
+        end
       end
 
       gem_rbi_path = ctx.absolute_path("sorbet/rbi/gems/#{name}@#{version}.rbi")
       FileUtils.cp(gem_rbi_path, "#{@project_path}/#{GEM_RBI_DIRECTORY}/")
+      @logger.success("Generated #{name}@#{version}.rbi")
 
       file = Bundler.read_file("/tmp/rbi/gems/#{name}/#{version}/Gemfile.lock")
       parser = Bundler::LockfileParser.new(file)
       spec = parser.specs.find { |spec| spec.name == name }
       spec.dependencies.each do |dependency|
         name = dependency.name
-        version = parser.specs.find { |spec| spec.name == name }.version.to_s
+        dependency_spec = parser.specs.find { |spec| spec.name == name }
+        next unless dependency_spec
+        version = dependency_spec.version.to_s
+        if has_local_rbi_for_gem_version?(name, version)
+            next # TODO display warning and inform of a `--force` option to regenerate even if it exists
+        end
+
         gem_rbi_path = ctx.absolute_path("sorbet/rbi/gems/#{name}@#{version}.rbi")
         FileUtils.cp(gem_rbi_path, "#{@project_path}/#{GEM_RBI_DIRECTORY}/")
+        @logger.success("Generated #{name}@#{version}.rbi")
       end
 
-      # TODO: Success logging
+      @logger.info("Successfully generated RBIs for #{spec.name}@#{spec.version} and its dependencies.")
 
       ctx.destroy
     end
