58135d35cc2cfd6243510adf40f108ae028bfe2e
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2021-06-17 12:41:19 -0400

    Order CLI commands alphebtically
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/rbi/cli.rb
+++ lib/rbi/cli.rb
@@ -12,25 +12,18 @@ module RBI
     class_option :quiet, type: :boolean, default: false, aliases: :q
     class_option :verbose, type: :boolean, default: false, aliases: :v
 
-    # TODO: Support triggering this command outside of root directory
-    desc "init", "Initialize a project by retrieving all gem RBIs from central repository"
-    option "mock-github-client", type: :boolean, default: false
-    def init
-      client = self.client(options["mock-github-client"])
-      client.init
-    end
-
     desc "clean", "Remove all gem RBIs from local project"
     def clean
       client = self.client
       client.clean
     end
 
-    desc "update", "Update local gem RBIs"
+    # TODO: Support triggering this command outside of root directory
+    desc "init", "Initialize a project by retrieving all gem RBIs from central repository"
     option "mock-github-client", type: :boolean, default: false
-    def update
+    def init
       client = self.client(options["mock-github-client"])
-      client.update
+      client.init
     end
 
     desc "generate foo 1.0.0", "Generates RBI for a given gem. To use Sorbet in your project, use `rbi update` instead"
@@ -44,6 +37,13 @@ module RBI
       path = options["path"]
       branch = options["branch"]
       generate_rbi(name, version: version, source: source, git: git, branch: branch, path: path)
+    end
+
+    desc "update", "Update local gem RBIs"
+    option "mock-github-client", type: :boolean, default: false
+    def update
+      client = self.client(options["mock-github-client"])
+      client.update
     end
 
     def self.exit_on_failure?
