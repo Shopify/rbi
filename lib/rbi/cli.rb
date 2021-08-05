dbf8fb9204d07d3d419cc6757cc63a756267875f
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2021-08-05 15:16:48 -0400

    TMP

--- lib/rbi/cli.rb
+++ lib/rbi/cli.rb
@@ -14,10 +14,40 @@ module RBI
       puts "RBI v#{RBI::VERSION}"
     end
 
+    desc 'show', 'Show RBI content'
+    # TODO format
+    # TODO options
+    def show(*paths)
+      # files = expand_paths(paths)
+      rbis = parse_files(paths)
+      # logger = self.logger
+      rbis.each do |file, rbi|
+        # puts logger.colorize("\n# #{file}\n", :light_black)
+        puts file
+        puts rbi.string(
+          # color: color?,
+          # max_len: 100
+        )
+      end
+    end
+
     # Utils
 
     def self.exit_on_failure?
       true
+    end
+
+    no_commands do
+      def parse_files(files)
+        # logger = self.logger
+
+        # index = 0
+        files.map do |file|
+          # logger.debug("Parsing #{file} (#{index}/#{files.size})")
+          # index += 1
+          [file, T.must(RBI::Parser.parse_file(file))]
+        end
+      end
     end
   end
 end
