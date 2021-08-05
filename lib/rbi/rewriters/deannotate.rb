4cfd533e092bbe5b81152687992be72caec2e4af
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2021-08-05 17:51:21 -0400

    Add Deannotate rewriter
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

---
+++ lib/rbi/rewriters/deannotate.rb
@@ -0,0 +1,45 @@
+# typed: strict
+# frozen_string_literal: true
+
+module RBI
+  module Rewriters
+    class Deannotate < Visitor
+      extend T::Sig
+
+      sig { params(annotation: String).void }
+      def initialize(annotation)
+        super()
+        @annotation = annotation
+      end
+
+      sig { override.params(node: T.nilable(Node)).void }
+      def visit(node)
+        case node
+        when Scope, Const, Attr, Method, TStructField, TypeMember
+          deannotate_node(node)
+        end
+        visit_all(node.nodes) if node.is_a?(Tree)
+      end
+
+      private
+
+      sig { params(node: NodeWithComments).void }
+      def deannotate_node(node)
+        return unless node.annotations.one?(@annotation)
+        node.comments.reject! do |comment|
+          comment.text == "@#{@annotation}"
+        end
+      end
+    end
+  end
+
+  class Tree
+    extend T::Sig
+
+    sig { params(annotation: String).void }
+    def deannotate!(annotation)
+      visitor = Rewriters::Deannotate.new(annotation)
+      visitor.visit(self)
+    end
+  end
+end
