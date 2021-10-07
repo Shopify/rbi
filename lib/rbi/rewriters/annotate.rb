23f63230203f530f49e5b6b1f85132d51e2eed21
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2021-10-07 09:16:45 -0400

    Make annotate rewriter more configurable
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/rbi/rewriters/annotate.rb
+++ lib/rbi/rewriters/annotate.rb
@@ -6,17 +6,21 @@ module RBI
     class Annotate < Visitor
       extend T::Sig
 
-      sig { params(annotation: String).void }
-      def initialize(annotation)
+      sig { params(annotation: String, annotate_scopes: T::Boolean, annotate_properties: T::Boolean).void }
+      def initialize(annotation, annotate_scopes: false, annotate_properties: false)
         super()
         @annotation = annotation
+        @annotate_scopes = annotate_scopes
+        @annotate_properties = annotate_properties
       end
 
       sig { override.params(node: T.nilable(Node)).void }
       def visit(node)
         case node
-        when Scope, Const, Attr, Method, TStructField, TypeMember
-          annotate_node(node)
+        when Scope
+          annotate_node(node) if @annotate_scopes || root?(node)
+        when Const, Attr, Method, TStructField, TypeMember
+          annotate_node(node) if @annotate_properties
         end
         visit_all(node.nodes) if node.is_a?(Tree)
       end
@@ -28,15 +32,25 @@ module RBI
         return if node.annotations.one?(@annotation)
         node.comments << Comment.new("@#{@annotation}")
       end
+
+      sig { params(node: Node).returns(T::Boolean) }
+      def root?(node)
+        parent = node.parent_tree
+        parent.is_a?(Tree) && parent.parent_tree.nil?
+      end
     end
   end
 
   class Tree
     extend T::Sig
 
-    sig { params(annotation: String).void }
-    def annotate!(annotation)
-      visitor = Rewriters::Annotate.new(annotation)
+    sig { params(annotation: String, annotate_scopes: T::Boolean, annotate_properties: T::Boolean).void }
+    def annotate!(annotation, annotate_scopes: false, annotate_properties: false)
+      visitor = Rewriters::Annotate.new(
+        annotation,
+        annotate_scopes: annotate_scopes,
+        annotate_properties: annotate_properties
+      )
       visitor.visit(self)
     end
   end
--- test/rbi/rewriters/annotate_test.rb
+++ test/rbi/rewriters/annotate_test.rb
@@ -5,7 +5,37 @@ require "test_helper"
 
 module RBI
   class AnnotateTest < Minitest::Test
-    def test_add_annotation_to_nodes
+    def test_add_annotation_to_root_nodes
+      rbi = RBI::Parser.parse_string(<<~RBI)
+        module A
+          class B
+            def m; end
+          end
+        end
+
+        class C < T::Struct
+          const :c, String
+        end
+      RBI
+
+      rbi.annotate!("test")
+
+      assert_equal(<<~RBI, rbi.string)
+        # @test
+        module A
+          class B
+            def m; end
+          end
+        end
+
+        # @test
+        class C < T::Struct
+          const :c, String
+        end
+      RBI
+    end
+
+    def test_add_annotation_to_all_scopes
       rbi = RBI::Parser.parse_string(<<~RBI)
         module A
           FOO = type_member
@@ -24,7 +54,103 @@ module RBI
         end
       RBI
 
-      rbi.annotate!("test")
+      rbi.annotate!("test", annotate_scopes: true)
+
+      assert_equal(<<~RBI, rbi.string)
+        # @test
+        module A
+          FOO = type_member
+
+          # @test
+          class B
+            attr_reader :a
+            def m1; end
+            def self.m2; end
+          end
+        end
+
+        # @test
+        class C < T::Struct
+          const :a, String
+          prop :b, String
+        end
+      RBI
+    end
+
+    def test_add_annotation_to_all_properties
+      rbi = RBI::Parser.parse_string(<<~RBI)
+        # Root scope are always annotated
+        module A
+          FOO = type_member
+
+          class B
+            attr_reader :a
+
+            def m1; end
+            def self.m2; end
+          end
+        end
+
+        # Root scope are always annotated
+        class C < T::Struct
+          const :a, String
+          prop :b, String
+        end
+      RBI
+
+      rbi.annotate!("test", annotate_properties: true)
+
+      assert_equal(<<~RBI, rbi.string)
+        # Root scope are always annotated
+        # @test
+        module A
+          # @test
+          FOO = type_member
+
+          class B
+            # @test
+            attr_reader :a
+
+            # @test
+            def m1; end
+
+            # @test
+            def self.m2; end
+          end
+        end
+
+        # Root scope are always annotated
+        # @test
+        class C < T::Struct
+          # @test
+          const :a, String
+
+          # @test
+          prop :b, String
+        end
+      RBI
+    end
+
+    def test_add_annotation_to_all_nodes
+      rbi = RBI::Parser.parse_string(<<~RBI)
+        module A
+          FOO = type_member
+
+          class B
+            attr_reader :a
+
+            def m1; end
+            def self.m2; end
+          end
+        end
+
+        class C < T::Struct
+          const :a, String
+          prop :b, String
+        end
+      RBI
+
+      rbi.annotate!("test", annotate_scopes: true, annotate_properties: true)
 
       assert_equal(<<~RBI, rbi.string)
         # @test
@@ -68,7 +194,7 @@ module RBI
         end
       RBI
 
-      rbi.annotate!("test")
+      rbi.annotate!("test", annotate_scopes: true, annotate_properties: true)
 
       assert_equal(<<~RBI, rbi.string)
         # @test
@@ -94,7 +220,7 @@ module RBI
         end
       RBI
 
-      rbi.annotate!("other")
+      rbi.annotate!("other", annotate_scopes: true, annotate_properties: true)
 
       assert_equal(<<~RBI, rbi.string)
         # @test
