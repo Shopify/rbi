b1d3f6c7e2854a503480fd567e1f40285893c286
Emily Samp <emily.samp@shopify.com>
2023-03-29 10:13:29 -0500

    Add lint versions visitor, test, and require

---
+++ lib/rbi/rewriters/lint_versions.rb
@@ -0,0 +1,10 @@
+# typed: strict
+# frozen_string_literal: true
+
+module RBI
+  module Rewriters
+    class LintVersions < Visitor
+      extend T::Sig
+    end
+  end
+end
---
+++ test/rbi/rewriters/lint_versions_test.rb
@@ -0,0 +1,9 @@
+# typed: true
+# frozen_string_literal: true
+
+require "test_helper"
+
+module RBI
+  class LintVersions < Minitest::Test
+  end
+end
