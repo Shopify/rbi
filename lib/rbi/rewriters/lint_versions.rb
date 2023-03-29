d026d3229ad4d0ac5e33ddf8eb7b55e652465463
Emily Samp <emily.samp@shopify.com>
2023-03-29 13:00:36 -0500

    Add support for linting invalid versions

--- lib/rbi/rewriters/lint_versions.rb
+++ lib/rbi/rewriters/lint_versions.rb
@@ -5,6 +5,80 @@ module RBI
   module Rewriters
     class LintVersions < Visitor
       extend T::Sig
+
+      class InvalidVersion < RBI::Error
+        extend T::Sig
+
+        sig { returns(Loc) }
+        attr_reader :location
+
+        sig { params(message: String, location: Loc).void }
+        def initialize(message, location)
+          super(message)
+          @location = location
+        end
+      end
+
+      OPERATOR_REGEX = T.let(/^(<|>|<=|>=|=)$/.freeze, Regexp)
+
+      sig { override.params(node: T.nilable(Node)).void }
+      def visit(node)
+        return unless node
+        return unless node.is_a?(NodeWithComments)
+
+        node.comments.each do |comment|
+          next unless comment.text.start_with?(/@version(-|\b)/)
+
+          text = comment.text.delete_prefix("@version").strip
+          versions = text.split(/, */)
+
+          if versions.empty?
+            raise InvalidVersion.new(
+              "Invalid version string `#{text}` in annotation `#{comment.text}`",
+              T.must(comment.loc)
+            )
+          end
+
+          versions.each do |version_line|
+            operator_str, version_str = version_line.split(" ")
+            version = parse_version(version_str, comment)
+            operator = parse_operator(operator_str, comment)
+          end
+        end
+
+        visit_all(node.nodes.dup) if node.is_a?(Tree)
+      end
+
+      private
+
+      sig { params(version_str: T.nilable(String), comment: Comment).returns(Gem::Version) }
+      def parse_version(version_str, comment)
+        unless version_str
+          raise InvalidVersion.new(
+            "Invalid version string `#{version_str}` in annotation `#{comment.text}`",
+            T.must(comment.loc)
+          )
+        end
+
+        Gem::Version.new(version_str)
+      rescue ArgumentError
+        raise InvalidVersion.new(
+          "Invalid version string `#{version_str}` in annotation `#{comment.text}`",
+          T.must(comment.loc)
+        )
+      end
+
+      sig {params(operator_str: T.nilable(String), comment: Comment).returns(String)}
+      def parse_operator(operator_str, comment)
+        unless OPERATOR_REGEX =~ operator_str
+          raise InvalidVersion.new(
+            "Invalid operator `#{operator_str}` in annotation `#{comment.text}`",
+            T.must(comment.loc)
+          )
+        end
+
+        T.must(operator_str)
+      end
     end
   end
 end
--- test/rbi/rewriters/lint_versions_test.rb
+++ test/rbi/rewriters/lint_versions_test.rb
@@ -5,5 +5,71 @@ require "test_helper"
 
 module RBI
   class LintVersions < Minitest::Test
+    def test_lint_versions_do_nothing
+      tree = Parser.parse_string(<<~RBI)
+        class Foo; end
+      RBI
+
+      Rewriters::LintVersions.new.visit(tree)
+    end
+
+    def test_lint_versions_does_not_raise_on_valid_version
+      tree = Parser.parse_string(<<~RBI)
+        # @version < 1.0.0-prerelease
+        class Foo; end
+      RBI
+
+      Rewriters::LintVersions.new.visit(tree)
+    end
+
+    def test_lint_versions_raises_on_invalid_version
+      tree = Parser.parse_string(<<~RBI)
+        # @version < abc
+        class Foo; end
+      RBI
+
+      err = assert_raises(Rewriters::LintVersions::InvalidVersion) do
+        Rewriters::LintVersions.new.visit(tree)
+      end
+
+      assert_equal("-:1:0-1:16", err.location.to_s)
+    end
+
+    def test_lint_versions_raises_on_invalid_operator
+      tree = Parser.parse_string(<<~RBI)
+        # @version ~> 1.0.0
+        class Foo; end
+      RBI
+
+      err = assert_raises(Rewriters::LintVersions::InvalidVersion) do
+        Rewriters::LintVersions.new.visit(tree)
+      end
+
+      assert_equal("-:1:0-1:19", err.location.to_s)
+    end
+
+    def test_lint_versions_raises_on_empty_version
+      tree = Parser.parse_string(<<~RBI)
+        # @version
+        class Foo; end
+      RBI
+
+      err = assert_raises(Rewriters::LintVersions::InvalidVersion) do
+        Rewriters::LintVersions.new.visit(tree)
+      end
+
+      assert_equal("-:1:0-1:10", err.location.to_s)
+    end
+
+    def test_lint_versions_ignore_invalid_annotation
+      tree = Parser.parse_string(<<~RBI)
+        # @version-invalid < 1.0.0
+        # @version_invalid < 1.0.0
+        # @versioninvalid < 1.0.0
+        class Foo; end
+      RBI
+
+      Rewriters::LintVersions.new.visit(tree)
+    end
   end
 end
