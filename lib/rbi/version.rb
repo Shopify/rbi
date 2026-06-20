619adc3e649dcea66e6ed469c1305c71a45e3869
Alexander Momchilov <alexander.momchilov@shopify.com>
2026-06-19 20:51:12 -0400

    Set faked version to `v0.3.15.backported`
    Co-authored-by: Alex Rocha <alex.rocha@shopify.com>

--- lib/rbi/version.rb
+++ lib/rbi/version.rb
@@ -2,5 +2,6 @@
 # frozen_string_literal: true
 
 module RBI
-  VERSION = "0.3.8"
+  # This version is actually based on RBI v0.3.8, but we need to compare as `>= 0.3.15` for our backport use case.
+  VERSION = "0.3.15.backported"
 end
